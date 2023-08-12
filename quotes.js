160a28987559390a7dacceda3e0fa2a6eb0300c4
Dhruv R Jain <dhruvrjain01@gmail.com>
2023-08-12 20:19:03 +0530

    Initial Commit

---
+++ quotes.js
@@ -0,0 +1,66 @@
+const quote=document.querySelector(".quote")
+const btn=document.querySelector("#new-quote")
+const person=document.querySelector(".person")
+
+quotes=[
+    {
+        quote: "Go to the f*ckin GYM",
+        person: "Papa Jones",
+    },
+    {
+        quote: "Wake up to reality",
+        person: "Madara Uchiha",
+    },
+    {
+        quote: "Power comes to a response of a need, not want",
+        person: "Son Goku",
+    },
+    {
+        quote: "Strong protect the weak and the weak in-turn become strong",
+        person: "Tanjiro Kamado",
+    },
+    {
+        quote: "Clan or the village ?",
+        person: "Itachi Uchiha",
+    },
+    {
+        quote: "Never forget the Good in you. The ones who love you know it.",
+        person: "Pooja",
+    },
+    {
+        quote: "The difference between a novice and a master is that a master has failed more times than a novice had tried.",
+        person: "Koro-Sensei",
+    },
+    {
+        quote:"I hate perfection. To be perfect is to be unable to improve any further.",
+        person:"Kurotsuchi Mayuri"
+    },
+    {
+        quote:"Every journey begins with a single step. We just have to have patience",
+        person:"Milly Thompson"
+    },
+    {
+        quote:"You've got two legs and a heartbeat. What's stopping you?",
+        person:"Inuyasha"
+    },
+    {
+        quote: "No amount of talent trumps hard work",
+        person:"Rin Matsuoke"
+    },
+    {
+        quote:"Fear is not evil. It tells you what weakness is. And once you know your weakness, you can become stronger as well as kinder.",
+        person: "Gildarts Clive"
+    },
+    {
+        quote:"The ticket to the future is always open",
+        person:"Vash The Stampede"
+    }
+]
+
+btn.addEventListener('click',function(){
+    
+    let rand=Math.floor(Math.random() * quotes.length)
+
+    quote.innerText = quotes[rand].quote;
+    person.innerText=quotes[rand].person
+})
